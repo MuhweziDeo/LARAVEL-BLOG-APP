2f67b715ce03e76431e389ef5f3c37e7ae134e7a
muhwezi deo <aggrey256@gmail.com>
2019-01-13 22:32:43 +0300

    setup basic routing and controller

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>{{config('app.name','blog-app')}}</title>
+</head>
+<body>
+	<h1>Index</h1>
+<p>Blade</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/','PagesController@index');
+
+Route::get('/about','PagesController@about');
+
+Route::get('/services','PagesController@services');
+
+// Route::get('users/{id}',function($id){
+// 	return $id;
+
+// });
+
+// Route::get('/about',function(){
+// 	return view('pages.about');
+
+// });
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
